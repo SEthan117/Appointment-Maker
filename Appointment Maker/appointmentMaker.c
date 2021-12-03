b920fcd286b5ecde50a1f634e4984731b6ea30db
Max Dabski <Mdabski6416@conestogac.on.ca>
2021-12-03 14:08:58 -0500

    Added getter and setter functions for appointment ADT

--- Appointment Maker/appointmentMaker.c
+++ Appointment Maker/appointmentMaker.c
@@ -18,3 +18,63 @@ P_APPT createAppointment(char year[], int date, int time, char description[])
 
 	return newAppt;
 }
+
+char* getYear(APPT appointment)
+{
+	return appointment.year;
+}
+
+int getDate(APPT appointment)
+{
+	return appointment.date;
+}
+
+int getTime(APPT appointment)
+{
+	return appointment.time;
+}
+
+char* getDescription(APPT appointment)
+{
+	return appointment.description;
+}
+
+P_APPT getNext(APPT appointment)
+{
+	return appointment.next;
+}
+
+P_APPT getPrevious(APPT appointment)
+{
+	return appointment.prev;
+}
+
+void setYear(P_APPT p_appointment, char year[])
+{
+	strcpy(p_appointment->year, year);
+}
+
+void setDate(P_APPT p_appointment, int date)
+{
+	p_appointment->date = date;
+}
+
+void setTime(P_APPT p_appointment, int time)
+{
+	p_appointment->time = time;
+}
+
+void setDescription(P_APPT p_appointment, char description[])
+{
+	strcpy(p_appointment->description, description);
+}
+
+void setNext(P_APPT p_appointment, P_APPT next)
+{
+	p_appointment->next = next;
+}
+
+void setPrevious(P_APPT p_appointment, P_APPT prev)
+{
+	p_appointment->prev = prev;
+}
--- Appointment Maker/appointmentMaker.h
+++ Appointment Maker/appointmentMaker.h
@@ -14,3 +14,43 @@ typedef struct appointment
 }APPT, *P_APPT;
 
 P_APPT createAppointment(char[], int, int, char[]);
+
+/* === Getter functions === */
+
+// Get appointment year
+char* getYear(APPT);
+
+// Get appointment date
+int getDate(APPT);
+
+// Get appointment time
+int getTime(APPT);
+
+// Get appointment description
+char* getDescription(APPT);
+
+// Get address of next appointment
+P_APPT getNext(APPT);
+
+// Get address of previous appointment
+P_APPT getPrevious(APPT);
+
+// Set appointment year
+void setYear(P_APPT, char[]);
+
+/* === Setter functions === */
+
+// Set appointment date
+void setDate(P_APPT, int);
+
+// Set appointment time
+void setTime(P_APPT, int);
+
+// Set appointment description
+void setDescription(P_APPT, char[]);
+
+// Set address of next appointment
+void setNext(P_APPT, P_APPT);
+
+// Set address of previous appointment
+void setPrevious(P_APPT, P_APPT);
